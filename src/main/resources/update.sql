00fa35081d022f0395158b9381c96d7952c30ac5
Zadorozhnyy Semen <zadorozhnyysemen@gmail.com>
2017-12-17 23:12:12 +0300

    e-notes/ insert script updated

--- src/main/resources/update.sql
+++ src/main/resources/update.sql
@@ -4,3 +4,14 @@ INSERT INTO Notebook (id, name) VALUES (3, 'awesome notebook');
 INSERT INTO Notebook (id, name) VALUES (4, 'test test');
 INSERT INTO Notebook (id, name) VALUES (5, 'just trying');
 INSERT INTO Notebook (id, name) VALUES (6, 'hi all nice notebook');
+
+INSERT INTO User (id, username, password, email, is_active) VALUES (1, 'Semen', 'qwerty', 'semen@epam.com', TRUE);
+
+INSERT INTO Note (id, title, content) VALUES (1, 'awesome title', 'incredible content');
+INSERT INTO Note (id, title, content) VALUES (2, 'awesome title 2', 'incredible content2 ');
+
+INSERT INTO Tags (id, name) VALUES (1, 'dummy tag');
+INSERT INTO Tags (id, name) VALUES (2, 'cats');
+INSERT INTO Tags (id, name) VALUES (3, 'fail');
+INSERT INTO Tags (id, name) VALUES (4, 'NB');
+INSERT INTO Tags (id, name) VALUES (5, 'work');
