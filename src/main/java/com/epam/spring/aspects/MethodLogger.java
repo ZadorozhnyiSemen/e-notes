e37fb523aab1b3cbe7b49af04b4edc7c52b07e6d
Zadorozhnyy Semen <zadorozhnyysemen@gmail.com>
2017-12-20 19:51:18 +0300

    e-notes/ log folder added to gitignore common logging removed log4j2 enabled Aspect corrected Aspect config added properties for logging added

--- src/main/java/com/epam/spring/aspects/MethodLogger.java
+++ src/main/java/com/epam/spring/aspects/MethodLogger.java
@@ -1,6 +1,7 @@
 package com.epam.spring.aspects;
 
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -10,23 +11,22 @@ import java.util.Arrays;
 @Aspect
 public class MethodLogger {
 
-  private final Logger logger = Logger.getLogger(MethodLogger.class);
+    private final Logger logger = LogManager.getLogger(this.getClass());
 
     @Around("within(com.epam.spring.service.impl.*)")
-  public Object around(ProceedingJoinPoint point) {
-    long start = System.currentTimeMillis();
-    Object result = null;
-    try {
-      result = point.proceed();
-    } catch (Throwable throwable) {
-      logger.debug("Method " + point.getSignature()
-                                    .toShortString() + " with args " + " " + Arrays.toString(
-          point.getArgs()), throwable);
-    }
+    public Object around(ProceedingJoinPoint point) {
+        long start = System.currentTimeMillis();
+        Object result = null;
+        try {
+            result = point.proceed();
+        } catch (Throwable throwable) {
+            logger.debug("Method " + point.getSignature()
+                    .toShortString() + " with args " + Arrays.toString(
+                    point.getArgs()), throwable);
+        }
 
-    logger.debug("Method " + point.getSignature()
-                                  .toShortString() + " with args " + " " + Arrays.toString(
-        point.getArgs()) + " spent time " + (System.currentTimeMillis() - start));
-    return result;
-  }
+        logger.debug("Method " + point.getSignature()
+                .toShortString() + "| Spent time " + (System.currentTimeMillis() - start) + "ms");
+        return result;
+    }
 }
