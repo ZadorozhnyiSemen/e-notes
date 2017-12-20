0da06e02987403cfa6aa7e6a3c74b51a09880d5b
Zadorozhnyy Semen <zadorozhnyysemen@gmail.com>
2017-12-20 19:03:26 +0300

    e-notes/ Aspect corrections

--- src/main/java/com/epam/spring/aspects/MethodLogger.java
+++ src/main/java/com/epam/spring/aspects/MethodLogger.java
@@ -1,17 +1,18 @@
 package com.epam.spring.aspects;
 
-import java.util.Arrays;
 import org.apache.log4j.Logger;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+
+import java.util.Arrays;
 
 @Aspect
 public class MethodLogger {
 
   private final Logger logger = Logger.getLogger(MethodLogger.class);
 
-  @Around("execution(* com.epam.spring.controller..*(..))")
+    @Around("within(com.epam.spring.service.impl.*)")
   public Object around(ProceedingJoinPoint point) {
     long start = System.currentTimeMillis();
     Object result = null;
