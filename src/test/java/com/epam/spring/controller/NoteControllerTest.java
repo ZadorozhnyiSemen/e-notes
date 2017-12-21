80476ee47ebea725958c9e53991f508ee4517b6c
Ruslan Lagoiko <ruslan_lagoiko@epam.com>
2017-12-21 23:47:37 +0300

    added getNotes_should_return_2_notes

--- src/test/java/com/epam/spring/controller/NoteControllerTest.java
+++ src/test/java/com/epam/spring/controller/NoteControllerTest.java
@@ -5,15 +5,18 @@ import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.epam.spring.service.NoteService;
+import java.nio.charset.Charset;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -55,6 +58,18 @@ public class NoteControllerTest {
   }
 
   @Test
+  public void getNotes_should_return_2_notes() throws Exception {
+    /// given
+    String url = "/api/notes";
+    int expectedSize = 2;
+
+    // when then
+    mockMvc.perform(get(url))
+           .andExpect(status().isOk())
+           .andExpect(jsonPath("$", hasSize(expectedSize)));
+  }
+
+  @Test
   public void deleteNote_should_delete_1_note() throws Exception {
     // given
     String url = "/api/notes/1";
@@ -70,14 +85,26 @@ public class NoteControllerTest {
   }
 
   @Test
-  public void getNotes_should_return_2_notes() throws Exception {
-    /// given
+  public void createNote_should_create_1_note() throws Exception {
+    // given
     String url = "/api/notes";
-    int expectedSize = 2;
+    String note = "{\n"
+        + "  \"title\": \"awesome title\",\n"
+        + "  \"content\": \"incredible content\",\n"
+        + "  \"tags\": []\n"
+        + "}";
+
+    MediaType mediaType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+        MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
+
+    int expectedSize = 3;
 
     // when then
-    mockMvc.perform(get(url))
-           .andExpect(status().isOk())
-           .andExpect(jsonPath("$", hasSize(expectedSize)));
+    mockMvc.perform(post(url).contentType(mediaType)
+                             .content(note))
+           .andExpect(status().isOk());
+    int actualSize = noteService.getAll()
+                                .size();
+    assertThat(actualSize, is(expectedSize));
   }
 }
